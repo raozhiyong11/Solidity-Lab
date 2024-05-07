ae0499d7897951356849877034b1aa213c996f48
raozhiyong111 <raozhiyong11@qq.com>
2024-05-07 10:54:31 +0800

    删除artifcts文件

---
+++ 00_开发环境搭建/让Remix访问本地文件.md
@@ -0,0 +1,21 @@
+
+
+官方参考文档`https://remix-ide.readthedocs.io/zh-cn/latest/remixd.html`
+
+```bash
+npm install @remix-project/remixd
+```
+
+查看版本
+```bash
+remixd -v
+```
+启动remixd
+```bash
+remixd -s F:\code\github\Solidity-Lab -u https://remix.ethereum.org/
+```
+![](images/2024-05-07-10-18-34.png)
+
+访问remix online页面，切换到本地：
+
+![](images/2024-05-07-10-15-07.png)
--- 01_第一个智能合约/HelloWorld.sol
+++ 01_第一个智能合约/HelloWorld.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
+//pragma solidity >=0.6.0 <0.9.0;
 
 contract HelloWorld{
+    string public message;
+    constructor(string memory _message){
+        message = _message;
+    }
+    function setMessage(string memory _message) public{
+        message = _message;
+    }
 
+
+    function getMessage() public view returns(string memory){
+        return message;
+    }
         
 }
